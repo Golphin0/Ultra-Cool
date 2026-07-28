8768c7a81d0c7ece2c188e8771d8ea70bdf6c8d7
Golphin0 <judahbrawn469@gmail.com>
2026-07-28 15:22:34 -0400

    Updated build.ps1 to use manifest.json for a more customizeable and extensive builder

--- build.ps1
+++ build.ps1
@@ -1,9 +1,8 @@
 Add-Type -AssemblyName PresentationFramework
 Add-Type -AssemblyName System.Windows.Forms
 
-$defaultBase   = Join-Path $PSScriptRoot "base.ps1"
-$defaultB64    = Join-Path $PSScriptRoot "base64.txt"
-$defaultOutput = Join-Path $PSScriptRoot "UltraCool.ps1"
+$defaultManifest = Join-Path $PSScriptRoot "Sources\manifest.json"
+$defaultOutput = $PSScriptRoot
 
 $xaml = @"
 <Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
@@ -61,58 +60,39 @@ $xaml = @"
 
             </Border>
 
-            <!-- Base -->
+            <!-- Manifest -->
             <Label Grid.Row="1"
                    Grid.Column="0"
                    VerticalAlignment="Center">
-                Base:
+                Manifest:
             </Label>
 
-            <TextBox Name="BaseFile"
+            <TextBox Name="ManifestFile"
                      Grid.Row="1"
                      Grid.Column="1"
                      Margin="5"/>
 
-            <Button Name="BrowseBase"
+            <Button Name="BrowseManifest"
                     Grid.Row="1"
                     Grid.Column="2"
                     Margin="5">
                 Browse...
             </Button>
 
-            <!-- Base64 -->
-            <Label Grid.Row="2"
-                   Grid.Column="0"
-                   VerticalAlignment="Center">
-                Base64:
-            </Label>
-
-            <TextBox Name="Base64File"
-                     Grid.Row="2"
-                     Grid.Column="1"
-                     Margin="5"/>
-
-            <Button Name="BrowseB64"
-                    Grid.Row="2"
-                    Grid.Column="2"
-                    Margin="5">
-                Browse...
-            </Button>
-
             <!-- Output -->
-            <Label Grid.Row="3"
+            <Label Grid.Row="2"
                    Grid.Column="0"
                    VerticalAlignment="Center">
                 Output:
             </Label>
 
             <TextBox Name="OutputFile"
-                     Grid.Row="3"
+                     Grid.Row="2"
                      Grid.Column="1"
                      Margin="5"/>
 
             <Button Name="BrowseOutput"
-                    Grid.Row="3"
+                    Grid.Row="2"
                     Grid.Column="2"
                     Margin="5">
                 Browse...
@@ -160,59 +140,46 @@ $xaml = @"
 $reader = New-Object System.Xml.XmlNodeReader ([xml]$xaml)
 $window = [Windows.Markup.XamlReader]::Load($reader)
 
-$BaseFile     = $window.FindName("BaseFile")
-$Base64File   = $window.FindName("Base64File")
+$ManifestFile   = $window.FindName("ManifestFile")
 $OutputFile   = $window.FindName("OutputFile")
 
-$BrowseBase   = $window.FindName("BrowseBase")
-$BrowseB64    = $window.FindName("BrowseB64")
+$BrowseManifest = $window.FindName("BrowseManifest")
 $BrowseOutput = $window.FindName("BrowseOutput")
 
 $ResetButton = $window.FindName("ResetButton")
 $CreateButton = $window.FindName("CreateButton")
 $CancelButton = $window.FindName("CancelButton")
 
-$BaseFile.Text   = $defaultBase
-$Base64File.Text = $defaultB64
+$ManifestFile.Text = $defaultManifest
 $OutputFile.Text = $defaultOutput
 
-$BrowseBase.Add_Click({
+
+$BrowseManifest.Add_Click({
     $d = New-Object System.Windows.Forms.OpenFileDialog
-    $d.Filter = "PowerShell Scripts (*.ps1)|*.ps1|All Files (*.*)|*.*"
-    $d.InitialDirectory = Split-Path $BaseFile.Text
+    $d.Filter = "Manifest Files (*.json)|*.json|All Files (*.*)|*.*"
+    $d.InitialDirectory = Split-Path $ManifestFile.Text
 
     if($d.ShowDialog() -eq "OK"){
-        $BaseFile.Text = $d.FileName
-    }
-})
-
-$BrowseB64.Add_Click({
-    $d = New-Object System.Windows.Forms.OpenFileDialog
-    $d.Filter = "Base64 Files (*.txt)|*.txt|All Files (*.*)|*.*"
-    $d.InitialDirectory = Split-Path $Base64File.Text
-
-    if($d.ShowDialog() -eq "OK"){
-        $Base64File.Text = $d.FileName
+        $ManifestFile.Text = $d.FileName
     }
 })
 
 $BrowseOutput.Add_Click({
-    $d = New-Object System.Windows.Forms.SaveFileDialog
-    $d.FileName = $OutputFile.Text
-    if($d.ShowDialog() -eq "OK"){
-        $OutputFile.Text = $d.FileName
+    $d = New-Object System.Windows.Forms.FolderBrowserDialog
+    $d.SelectedPath = $OutputFile.Text
+
+    if ($d.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+        $OutputFile.Text = $d.SelectedPath
     }
 })
 
 $ResetButton.Add_Click({
-    $BaseFile.Text   = $defaultBase
-    $Base64File.Text = $defaultB64
+    $ManifestFile.Text = $defaultManifest
     $OutputFile.Text = $defaultOutput
 })
 
 $CreateButton.Add_Click({
-    $script:baseFile   = $BaseFile.Text
-    $script:b64File    = $Base64File.Text
+    $script:manifestFile    = $ManifestFile.Text
     $script:outFile    = $OutputFile.Text
     $window.DialogResult = $true
 })
@@ -225,54 +192,113 @@ if(-not $window.ShowDialog()){
     return
 }
 
-Write-Verbose "Base File   : $baseFile" -Verbose
-Write-Verbose "Base64 File : $b64File" -Verbose
+Write-Verbose "Manifest File : $manifestFile" -Verbose
 Write-Verbose "Output File : $outFile" -Verbose
 
-if (!(Test-Path $baseFile)) {
-    Write-Verbose "Base file not found." -Verbose
-    Write-Host "$baseFile not found." -ForegroundColor Red
-    pause
-    exit 1
-}
 
-if (!(Test-Path $b64File)) {
-    Write-Verbose "Base64 file not found." -Verbose
-    Write-Host "$b64File not found." -ForegroundColor Red
+if (!(Test-Path $manifestFile)) {
+    Write-Verbose "Manifest file not found." -Verbose
+    Write-Host "$manifestFile not found." -ForegroundColor Red
     pause
     exit 1
 }
 
 Write-Verbose "Reading input files..." -Verbose
 
-$base = Get-Content $baseFile -Raw
-$lines = Get-Content $b64File
+$lines = Get-Content $manifestFile
 
-if ($lines.Count % 2 -ne 0) {
-    Write-Verbose "Invalid base64.txt format." -Verbose
-    Write-Host "base64.txt must contain an even number of lines." -ForegroundColor Red
-    pause
-    exit 1
+
+New-Item -Force -ItemType Directory -Path (Join-Path $env:TEMP "UCBuildTemp")
+Write-Verbose "Processing variables..." -Verbose
+
+$json = $lines | ConvertFrom-Json
+$basefiles = @()
+
+foreach ($item in $json) {
+    $name = $item.name
+    if ($name -eq "basefiles") {
+        foreach ($file in $item.files) {
+            $basefiles += $file
+        }
+    } elseif ($name -eq "sources") {
+        foreach ($file in $item.files) {
+            Write-Verbose "Processing $($file.name)..." -Verbose
+            $format = $file.format
+            $archive = $file.archive
+            $source = $file.source
+            $target = $file.target -replace '\[\[CURRENTBUILDS\]\]', (Join-Path $env:TEMP "UCBuildTemp")
+            $fromSources = $false
+            if ($target -like "@*") {
+                $target = $target.TrimStart('@')
+                $fromSources = $true
+            }
+            if ($fromSources -eq $true) {
+                $target = $($basefiles | Where-Object name -eq $target | Select-Object -First 1).path
+                Write-Verbose "Target file: $target" -Verbose
+                Write-Verbose "UHUH" -Verbose
+            }
+            $varname = $file.varname
+            $source = $source -replace '\[\[CURRENTBUILDS\]\]', (Join-Path $env:TEMP "UCBuildTemp")
+            $tempplace = Join-Path $env:TEMP (Join-Path "UCBuildTemp" $file.tempplace)
+            $out = ""
+            Set-Location (Join-Path $PSScriptRoot "Sources")
+
+            if ($format -eq "base64") {
+                switch ($archive) {
+                    $true {
+                        Write-Verbose "$source" -Verbose
+                        Write-Verbose "$(Join-Path $source "*")" -Verbose
+                        $tempZip = "$(Join-Path $env:TEMP (Join-Path "UCBuildTemp" ("tempzip$(Get-Random -Min 1000 -Max 10000)" + ".zip")))"
+                        Write-Verbose "$tempZip" -Verbose
+                        Compress-Archive -Path (Join-Path $source "*") -DestinationPath $tempZip
+                        $rawData = [IO.File]::ReadAllBytes($tempZip)
+                        Remove-Item $tempZip
+                    }
+                    $false {
+                        $rawData = [IO.File]::ReadAllBytes($source)
+                    }
+                }
+                $base64Data = [Convert]::ToBase64String($rawData)
+                $out += "`$$varname = `"$base64Data`""
+            } elseif ($format -eq "directTable") {
+                $source = Join-Path $PSScriptRoot (Join-Path "Sources" $source)
+                $rawData = Get-Content $source -Raw
+                $out += "`$$varname = $rawData"
+            } elseif ($format -eq "carbonCopy") {
+                $out = ""
+            }
+            # $type = ($basefiles | Where-Object name -eq $target | Select-Object -First 1).addtype
+
+            Write-Verbose "Processing target file: $(Test-Path $target)" -Verbose
+            $baseData = Get-Content $target -Raw
+            if ($format -ne "carbonCopy") {$out += "`r`n"}
+            $out += $baseData
+            
+            New-Item -ItemType File -Path $tempplace -Force
+            Set-Content $tempplace -Value $out
+            Write-Verbose "$(Test-Path $tempplace)" -Verbose
+        }
+
+        
+        Write-Verbose "$tempplace" -Verbose
+    }
 }
 
-$out = ""
-$out += "# Base64 Strings`r`n"
-
-Write-Verbose "Processing base64 variables..." -Verbose
-
-for ($i = 0; $i -lt $lines.Count; $i += 2) {
-    $name = $lines[$i].Trim()
-    $value = $lines[$i + 1]
-    Write-Verbose "Embedding variable '$name'" -Verbose
-    $out += "`$$name = `"$value`"`r`n"
-}
-
-$out += "`r`n"
-$out += $base.TrimStart()
 
 Write-Verbose "Writing output..." -Verbose
 
-Set-Content $outFile -Value $out -Encoding UTF8
+foreach ($file in $json) {
+    $name = $file.name
+    if ($name -eq "outputs") {
+        foreach ($output in $file.files) {
+            $source = $output.source -replace '\[\[CURRENTBUILDS\]\]', (Join-Path $env:TEMP "UCBuildTemp")
+            $path = Join-Path $outFile $output.path
+            $data = Get-Content $source -Raw
+            New-Item -ItemType File -Path $path -Force
+            Set-Content $path -Value $data
+        }
+    }
+}
 
 Write-Verbose "Done." -Verbose
 Write-Host "Created $outFile" -ForegroundColor Green
